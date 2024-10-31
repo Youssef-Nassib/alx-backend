86220b85bce8e70c6e486881165e7f500ac01a7f
YoussefNassib <wissal2015.nassib@gmail.com>
2024-10-31 00:06:40 +0000

    3-hypeermedia del pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -34,7 +34,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
+        """get a page of data.
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
